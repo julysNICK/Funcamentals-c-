0f290fa69ac90a950c8f6a2904039742de756563
julysNICK <julystube@gmail.com>
2025-10-19 12:00:26 -0300

    Add project files and build artifacts for Exercise2
    - Created Exercise2.dll and Exercise2.ref.dll for the Exercise2 project.
    - Added project.assets.json and related NuGet configuration files for package management.
    - Included sourcelink.json for debugging support in ReadUser project.
    - Updated project metadata and assembly attributes.

---
+++ Exercise2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Enter your first name: ");
+string firstName = Console.ReadLine().Trim();
+
+Console.Write("Enter your last name: ");
+string lastName = Console.ReadLine().Trim();
+
+while (string.IsNullOrWhiteSpace(firstName) || string.IsNullOrWhiteSpace(lastName))
+{
+    Console.WriteLine("Both first name and last name must be provided. Please try again.");
+
+    Console.Write("Enter your first name: ");
+    firstName = Console.ReadLine().Trim();
+
+    Console.Write("Enter your last name: ");
+    lastName = Console.ReadLine().Trim();
+}
+
+
+Console.WriteLine($"Hello, {firstName} {lastName}!");
