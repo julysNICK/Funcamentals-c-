9626b88c5d6f349cb09299c94a4eb1a1c942cfff
julysNICK <julystube@gmail.com>
2025-10-19 12:41:59 -0300

    Add initial project files and configurations for Exercise4
    - Created Exercise4.dll and Exercise4.ref.dll for the project.
    - Added project.assets.json to manage package dependencies.
    - Included project-specific NuGet configuration files (g.props, g.targets).
    - Established project structure for .NET 8.0 targeting.
    - Configured assembly attributes and metadata for the Exercise4 project.

--- Exercise2/obj/Debug/net8.0/Exercise2.AssemblyInfo.cs
+++ Exercise2/obj/Debug/net8.0/Exercise2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercise2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f290fa69ac90a950c8f6a2904039742de756563")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37c931023409d08cee984011fa84184092f32fbf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercise2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercise2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Exercise4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Insert a word: ");
+string word1 = Console.ReadLine();
+string wordWithoutSpaces = word1.Replace(" ", "");
+
+if (word1.Length == 0)
+{
+    Console.WriteLine("You did not enter a word.");
+    return;
+}
+
+int length1 = wordWithoutSpaces.Length;
+int length2 = word1.Length;
+
+Console.WriteLine($"The length of the word without spaces is {length1}.");
+Console.WriteLine($"The length of the word with spaces is {length2}.");
+
+Console.ReadLine();
+
